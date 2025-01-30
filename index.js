d70c2a4f523a252fab23c74856b7aca892f49c8b
username_anjastabuni <reblytabuni2004@gmail.com>
2025-01-30 22:20:32 +0700

    setup project

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.set()
+// connect to mongodb
+mongoose
+  .connect("mongodb://127.0.0.1/shop_db")
+  .then((result) => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// roots
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("shop app listening on http://127.0.0.1:3000");
+});
